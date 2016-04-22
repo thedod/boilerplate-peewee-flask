93f1928a224474ba2b74abbd1b90049e6ece4e40
The Dod <unclezzzen@gmail.com>
2016-04-22 10:39:29 +0300

    cleanup init_local

--- init_local.sh
+++ init_local.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo APPLICATION_SITE_NAME=\"$SITE_NAME\" > .env
-echo APPLICATION_SECRET_KEY=$(python2 randme.py) >> .env
+echo APPLICATION_SECRET_KEY=$(python2 randme.py) > .env
 echo APPLICATION_SECURITY_PASSWORD_SALT=$(python2 randme.py) >> .env
 echo APPLICATION_DATABASE=sqlite:///dev.db >> .env
